2312131f5f05921f7c9cbc4aba9d9c470200235d
amrutha <amrutha@intersmart.in>
2025-10-13 19:21:42 +0530

    service page

--- src/components/features/service/OurTreatmentsection.jsx
+++ src/components/features/service/OurTreatmentsection.jsx
@@ -1,144 +1,3 @@
-// "use client";
-// import React, { useState } from "react";
-// import Image from "next/image";
-// import { motion } from "framer-motion";
-// import { Swiper, SwiperSlide, useSwiperSlide } from "swiper/react";
-// import { Thumbs, EffectFade } from "swiper/modules";
-// import "swiper/css";
-// import "swiper/css/thumbs";
-// import "swiper/css/effect-fade";
-// import { Heading } from "@/components/layout/Heading";
-// import { Text } from "@/components/layout/Text";
-
-// const treatmentData = [
-//     {
-//         title: "Systemic therapy",
-//         list: ["Step 1", "Step 2", "Step 3", "Step 4"],
-//     },
-//     {
-//         title: "Targeted therapy",
-//         list: ["Step A", "Step B", "Step C"],
-//     },
-//     {
-//         title: "Immunotherapy",
-//         list: ["Step X", "Step Y", "Step Z"],
-//     },
-// ];
-
-// export default function OurTreatmentsection({
-//     title,
-//     description,
-// }) {
-//     const [thumbsSwiper, setThumbsSwiper] = useState(null);
-//     const [activeIndex, setActiveIndex] = useState(0);
-
-//     return (
-//         <section className="w-full py-[80px] sm:py-[60px] xl:py-[80px] 3xl:py-[100px] relative z-0">
-//             <div className="container container-sm flex flex-wrap">
-//                 {/* Left Column */}
-//                 <div className="w-full md:w-[60%] xl:w-[50%] md:pr-[50px] mb-[30px]">
-//                     <motion.div
-//                         className="w-full p-[10px]"
-//                         initial={{ opacity: 0, x: -50 }}
-//                         whileInView={{ opacity: 1, x: 0 }}
-//                         transition={{ duration: 0.8 }}
-//                     >
-//                         <div className="!font-medium !text-[#101010] mb-[5px] 3xl:mb-[10px]">About Skyline</div>
-//                         <h2 className="text-3xl font-bold">{title}</h2>
-//                         <p className="line-clamp-4">{description}</p>
-//                     </motion.div>
-
-//                     {/* Thumbnails */}
-//                     <div className="mt-6">
-//                         <Swiper
-//                             modules={[Thumbs]}
-//                             onSwiper={setThumbsSwiper}
-//                             watchSlidesProgress
-//                             spaceBetween={10}
-//                             slidesPerView={4}
-//                             direction="vertical"
-//                             className="h-[300px]"
-//                             onSlideChange={(swiper) => setActiveIndex(swiper.activeIndex)} // <-- track active index
-//                         >
-//                             {treatmentData?.map((item, index) => (
-//                                 <SwiperSlide key={index}>
-//                                     <div
-//                                         className={`flex items-center justify-between w-full px-4 py-3 border-b border-[#00335B] cursor-pointer hover:bg-gray-50 transition ${activeIndex === index ? "bg-gray-100" : ""
-//                                             }`}
-//                                         onClick={() => thumbsSwiper?.slideTo(index)} 
-//                                     >
-//                                         <div
-//                                             className={`text-[12px] 2xl:text-[16px] 3xl:text-[18px] font-medium text-center md:text-start w-[calc(100%-15px)] mb-0 ${activeIndex === index ? "text-blue-500" : "text-gray-700"
-//                                                 }`}
-//                                         >
-//                                             {item.title}
-//                                         </div>
-//                                         <span className="w-[15px] h-[15px] flex items-center justify-center">
-//                                             <Image
-//                                                 src="/images/arrw.svg"
-//                                                 alt="icon"
-//                                                 width={13}
-//                                                 height={13}
-//                                                 className="w-full h-auto object-contain aspect-square"
-//                                             />
-//                                         </span>
-//                                     </div>
-//                                 </SwiperSlide>
-//                             ))}
-//                         </Swiper>
-
-//                     </div>
-//                 </div>
-
-//                 {/* Right Column */}
-//                 <div className="w-full md:w-[40%] xl:w-[50%]">
-//                     <div className="w-full h-full relative p-[60px] overflow-hidden bg-base2">
-//                         <div className="w-[42%] h-auto absolute right-0 top-0 -z-1">
-//                             <Image
-//                                 src="/images/traetmentBg.png"
-//                                 alt="about icon"
-//                                 width={670}
-//                                 height={580}
-//                                 className="w-full h-auto aspect-square"
-//                             />
-//                         </div>
-//                         <Swiper
-//                             modules={[Thumbs, EffectFade]}
-//                             watchSlidesProgress
-//                             effect="fade"
-//                             thumbs={{ swiper: thumbsSwiper }}
-//                             spaceBetween={10}
-//                             slidesPerView={1}>
-//                             {treatmentData?.map((item, index) => (
-//                                 <SwiperSlide key={index} className="bg-[#00335B] opacity-0 transition-opacity duration-500 swiper-slide-active:opacity-100">
-//                                     <div className="text-white relative z-3">
-//                                         <Heading
-//                                             as="h4"
-//                                             size="heading4"
-//                                             className="text-center xs:text-start font-medium text-white mb-[35px]"
-//                                         >
-//                                             {item.title}
-//                                         </Heading>
-//                                         <ul>
-//                                             {item.list.map((list, liIndex) => (
-//                                                 <li key={liIndex} className="relative text-[12px] 2xl:text-[14px] 3xl:text-[18px] text-[#212121] font-normal pl-[25px] mb-[8px]
-//                                                     before:absolute before:top-[4px] before:left-0 before:w-[15px]  before:2xl:w-[17px] before:h-[15px] before:2xl:h-[17px] before:flex before:align-items-center
-//                                                     before:bg-[url('/images/teartIcon.svg')] before:bg-no-repeat before:bg-contain before:content-['']">
-
-//                                                     {list}
-//                                                 </li>
-//                                             ))}
-//                                         </ul>
-//                                     </div>
-//                                 </SwiperSlide>
-//                             ))}
-//                         </Swiper>
-//                     </div>
-//                 </div>
-//             </div>
-//         </section>
-//     );
-// }
 "use client";
 import React, { useState } from "react";
 import Image from "next/image";
@@ -165,43 +24,40 @@ export default function OurTreatmentsection({ title, description }) {
         <section className="w-full py-[80px] sm:py-[60px] xl:py-[80px] 3xl:py-[100px] relative z-0">
             <div className="container container-sm flex flex-wrap">
                 {/* Left Column */}
-                <div className="w-full md:w-[60%] xl:w-[50%] md:pr-[50px] mb-[30px]">
+                <div className="w-full sm:w-[55%] xl:w-[50%] sm:pr-[60px] lg:pr-80px] xl:pr-[100px] 3xl:pr-[150px] mb-3 sm:mb-0">
                     <motion.div
-                        className="w-full p-[10px]"
+                        className="w-full"
                         initial={{ opacity: 0, x: -50 }}
                         whileInView={{ opacity: 1, x: 0 }}
                         transition={{ duration: 0.8 }}
                     >
-                        <div className="!font-medium !text-[#101010] mb-[5px] 3xl:mb-[10px]">About Skyline</div>
-                        <h2 className="text-3xl font-bold">{title}</h2>
-                        <p className="line-clamp-4">{description}</p>
+                        <div className="text-[11px] sm:text-[11px] lg:text-[12px] xl:text-[14px] 2xl:text-[16px] 3xl:text-[18px] font-normal tracking-[3.06px] text-center xs:text-start uppercase text-base1 mb-[6px] lg:mb-[10px]">our treatments</div>
+                        <h2 className="text-3xl font-unna text-center xs:text-start font-bold">{title}</h2>
                     </motion.div>
 
                     {/* Thumbnails */}
-                    <div className="mt-6">
+                    <div className="mt-[30px]">
                         <Swiper
                             modules={[Thumbs]}
                             onSwiper={setThumbsSwiper}
                             watchSlidesProgress
-                            spaceBetween={10}
+                            spaceBetween={0}
                             slidesPerView={4}
                             direction="vertical"
-                            className="h-[300px]"
+                            className="h-[165px] xl:h-[175px] 2xl:h-[240px] 3xl:h-[320px]"
                         >
                             {treatmentData.map((item, index) => (
-                                <SwiperSlide key={index}>
+                                <SwiperSlide key={index} className={`${index === treatmentData.length - 1 ? "!mb-0" : ""}`}>
                                     <div
-                                        className={`group flex items-center justify-between w-full px-4 py-3 border-b border-[#919193] cursor-pointer hover:text-base2 hover:border-[#00335B] transition
-                      ${activeIndex === index ? "" : ""}`}
-                                        onClick={() => thumbsSwiper?.slideTo(index)}
-                                    >
+                                        className={`group flex items-center justify-between w-full px-0 py-[6px] sm:py-[10px] 2xl:py-[15px] border-b border-[#919193] cursor-pointer hover:text-base2 hover:border-[#00335B] transition
+                                    ${activeIndex === index ? "" : ""}`}
+                                        onClick={() => thumbsSwiper?.slideTo(index)}>
                                         <div
-                                            className={`text-[12px] 2xl:text-[16px] 3xl:text-[18px] font-medium text-center md:text-start text-[#919193] w-[calc(100%-15px)] mb-0 
-                        ${activeIndex === index ? "text-base2" : "border-[#00335B]"} group-hover:text-base2`}
-                                        >
+                                            className={`text-[12px] 2xl:text-[16px] 3xl:text-[18px] leading-normal font-medium text-start  text-[#919193] w-[calc(100%-10px)] 2xl:w-[calc(100%-15px)] mb-0 
+                                        ${activeIndex === index ? "text-base2" : "border-[#00335B]"} group-hover:text-base2`} >
                                             {item.title}
                                         </div>
-                                        <span className="w-[15px] h-[15px] flex items-center justify-center">
+                                        <span className="w-[10px] xl:w-[15px] h-[10px] xl:h-[15px] flex items-center justify-center">
                                             <Image
                                                 src={activeIndex === index ? "/images/arrw-active.svg" : "/images/arrw.svg"}
                                                 alt="icon"
@@ -218,8 +74,8 @@ export default function OurTreatmentsection({ title, description }) {
                 </div>
 
                 {/* Right Column */}
-                <div className="w-full md:w-[40%] xl:w-[50%] overflow-hidden">
-                    <div className="w-full h-full relative p-[60px] overflow-hidden bg-base2">
+                <div className="w-full sm:w-[45%] xl:w-[50%] overflow-hidden">
+                    <div className="w-full h-full relative p-[15px] sm:p-[25px] lg:p-[30px] xl:p-[40px] 2xl:p-[50px] 3xl:p-[60px] overflow-hidden rounded-[6px] bg-base2">
                         <div className="w-70%] h-auto absolute -right-[10%] top-0 z-2">
                             <Image
                                 src="/images/traetmentBg.png"
@@ -247,7 +103,7 @@ export default function OurTreatmentsection({ title, description }) {
                                         <Heading
                                             as="h4"
                                             size="heading4"
-                                            className="text-center xs:text-start font-medium text-white mb-[35px]"
+                                            className="text-start font-medium text-white mb-[35px]"
                                         >
                                             {item.title}
                                         </Heading>
@@ -255,9 +111,9 @@ export default function OurTreatmentsection({ title, description }) {
                                             {item.list.map((list, liIndex) => (
                                                 <li
                                                     key={liIndex}
-                                                    className="relative text-[12px] 2xl:text-[14px] 3xl:text-[18px] text-white font-normal pl-[25px] mb-[8px]
-                            before:absolute before:top-[4px] before:left-0 before:w-[15px]  before:2xl:w-[17px] before:h-[15px] before:2xl:h-[17px] before:flex before:align-items-center
-                            before:bg-[url('/images/teartIcon.svg')] before:bg-no-repeat before:bg-contain before:content-['']"
+                                                    className="relative text-[12px] 2xl:text-[14px] 3xl:text-[18px] text-white font-normal pl-[28px] mb-[8px]
+                                                        before:absolute before:top-[4px] before:left-0 before:w-[15px]  before:2xl:w-[17px] before:h-[15px] before:2xl:h-[17px] before:flex before:align-items-center
+                                                        before:bg-[url('/images/teartIcon.svg')] before:bg-no-repeat before:bg-contain before:content-['']"
                                                 >
                                                     {list}
                                                 </li>
