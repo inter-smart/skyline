04071b927760266812f59bf3fdf141a59cb23bd2
Nikhil-Govind-Inter <nikhil@intersmart.in>
2025-10-27 12:04:56 +0530

    appointment section

--- src/components/features/home/AppointmentSection.jsx
+++ src/components/features/home/AppointmentSection.jsx
@@ -2,64 +2,81 @@
 
 import Image from "next/image";
 import { motion } from "framer-motion";
+import { renderHtml } from "@/utils/parseHtml";
+import Link from "next/link";
 
-export default function AppointmentSection({service_cms, bannerImage}) {
+export default function AppointmentSection({
+  sub_title,
+  title,
+  description,
+  path,
+  alt,
+  button_text,
+  button_link,
+  bannerImage,
+}) {
   // Fade-up animation
   const fadeUp = {
     hidden: { opacity: 0, y: 30 },
-    visible: { opacity: 1, y: 0, transition: { duration: 0.8, ease: "easeOut" } },
+    visible: {
+      opacity: 1,
+      y: 0,
+      transition: { duration: 0.8, ease: "easeOut" },
+    },
   };
 
   return (
     <section className="relative w-full h-full py-[90px] xl:py-[110px] 2xl:py-[130px] 3xl:py-[165px]">
       {/* Background image */}
+
       <Image
-        src={bannerImage }
+        src={path}
         className="object-cover absolute top-0 left-0 w-full h-full"
         priority
         width={1920}
         height={460}
-        alt={service_cms?.section3_image_alt_text_value}
+        alt={alt}
       />
 
       <div className="container w-full h-full flex items-center">
         <motion.div
           className="max-w-[470px] 2xl:max-w-[600px] 3xl:max-w-[700px] relative"
           initial="hidden"
-          whileInView="visible"         
-          viewport={{ once: true, amount: 0.3 }}  
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.3 }}
         >
           <motion.p
             className="text-[10px] xl:text-[12px] 2xl:text-[14px] 3xl:text-[18px] text-white uppercase font-normal tracking-[2px] mb-[15px] 2xl:mb-[20px] 3xl:mb-[30px]"
             variants={fadeUp}
           >
-            Booking
+            {sub_title}
           </motion.p>
 
           <motion.h2
             className="text-[30px] lg:text-[35px] xl:text-[44px] 2xl:text-[53px] 3xl:text-[66px] text-white font-normal font-unna capitalize leading-[30px] lg:leading-[35px] xl:leading-[44px] 2xl:leading-[53px] 3xl:leading-[66px] tracking-wider mb-[20px] xl:mb-[25px]"
             variants={fadeUp}
           >
-            Book Your <br />
-            Appointment Today
+            {title}
           </motion.h2>
 
-          <motion.p
+          <motion.div
             className="text-[12px] 2xl:text-[14px] 3xl:text-[18px] text-white font-normal max-w-[75%] mb-[25px] 2xl:mb-[30px] 3xl:mb-[40px]"
             variants={fadeUp}
           >
-            Your health and well-being are our top priorities. Experience world-class healthcare with a personal touch in the heart of Coventry.
-          </motion.p>
+            {renderHtml(description)}
+          </motion.div>
 
-          <motion.button
-            className="btn-base1 hover min-w-[135px] xl:min-w-[170px] 2xl:min-w-[200px] 3xl:min-w-[250px] tracking-wide"
-            aria-label="appointment"
-            variants={fadeUp}
-            whileHover={{ scale: 1.05 }}
-            whileTap={{ scale: 0.95 }}
-          >
-            Book an Appointment
-          </motion.button>
+          <Link href={button_link || "#"}>
+            <motion.button
+              className="btn-base1 hover min-w-[135px] xl:min-w-[170px] 2xl:min-w-[200px] 3xl:min-w-[250px] tracking-wide"
+              aria-label="appointment"
+              variants={fadeUp}
+              whileHover={{ scale: 1.05 }}
+              whileTap={{ scale: 0.95 }}
+            >
+              {button_text || "Book an Appointment"}
+            </motion.button>
+          </Link>
         </motion.div>
       </div>
     </section>
