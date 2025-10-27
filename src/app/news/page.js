8a1144a314a5fa87f6c35c78c5b09d61f8f6313b
afsalmern <afsal@intersmart.in>
2025-10-27 16:06:56 +0530

    bug fix

--- src/app/news/page.js
+++ src/app/news/page.js
@@ -2,17 +2,17 @@ import InnerBanner from "@/components/common/InnerBanner";
 import NewsSection from "@/components/features/News/NewsSection";
 
 export default function Page() {
-    return (
-        <>
-            <InnerBanner
-                img="/images/newsbanner.jpg"
-                alt="NEWS"
-                subTitle="NEWS"
-                Title="News & Insights"
-                description="Be part of a hospital dedicated to excellence, compassion, and innovation."
-                 />
+  return (
+    <>
+      <InnerBanner
+        img="/images/newsbanner.jpg"
+        alt="NEWS"
+        subTitle="NEWS"
+        Title="News & Insights"
+        description="Be part of a hospital dedicated to excellence, compassion, and innovation."
+      />
 
-                <NewsSection />
-        </>
-    )
+      <NewsSection />
+    </>
+  );
 }
--- src/components/features/about/AwardSection.jsx
+++ src/components/features/about/AwardSection.jsx
@@ -7,8 +7,7 @@ import { Navigation, Autoplay } from "swiper/modules";
 import "swiper/css";
 import "swiper/css/navigation";
 import { Heading, SubTitle } from "@/components/layout/Heading";
-import parse from 'html-react-parser';
-
+import { renderHtml } from "@/utils/parseHtml";
 
 export default function AwardSection({ about_cms, accreditation }) {
   return (
@@ -16,22 +15,14 @@ export default function AwardSection({ about_cms, accreditation }) {
       <div className="container">
         <div className="flex flex-wrap">
           <div className="w-full md:w-1/2">
-            <SubTitle
-              size="SubTitle"
-              as="div"
-              className="!mb-[10px] 3xl:!mb-[15px] leading-none"
-            >
+            <SubTitle size="SubTitle" as="div" className="!mb-[10px] 3xl:!mb-[15px] leading-none">
               {about_cms?.section7_pre_title}
             </SubTitle>
-            <Heading
-              size="heading1"
-              as="div"
-              className="leading-none !mb-[15px]"
-            >
+            <Heading size="heading1" as="div" className="leading-none !mb-[15px]">
               {about_cms?.section7_title}
             </Heading>
             <div className="md:max-w-[300px] lg:max-w-[340px] xl:max-w-[420px] 2xl:max-w-[500px] 3xl:max-w-[640px]">
-              <p>{parse(about_cms?.section7_description)}</p>
+              <p>{about_cms?.section7_description && renderHtml(about_cms?.section7_description)}</p>
             </div>
           </div>
           <div className="w-full md:w-1/2 lg:pl-40px">
@@ -52,11 +43,7 @@ export default function AwardSection({ about_cms, accreditation }) {
             >
               {accreditation?.map((item, index) => (
                 <SwiperSlide>
-                  <Link
-                    href="#"
-                    className="flex flex-col relative aspect-[360/250]"
-                    aria-label="consultants"
-                  >
+                  <Link href="#" className="flex flex-col relative aspect-[360/250]" aria-label="consultants">
                     <div className="bg-white flex items-center justify-center rounded-[5px] overflow-hidden z-0 w-full h-full border border-[#E3E5E8] ">
                       <Image
                         src={item.image_value}
