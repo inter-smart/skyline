85134d4db71df7afcb2cf6ed1aa5b64d9074e027
Nikhil-Govind-Inter <nikhil@intersmart.in>
2025-10-28 09:47:53 +0530

    Contact us page faq section

--- src/components/common/InnerBanner.jsx
+++ src/components/common/InnerBanner.jsx
@@ -55,13 +55,13 @@ export default function InnerBanner({ img, subTitle, Title, description, alt, sh
               {renderHtml(Title) | ""}
             </motion.h2>
 
-            <motion.p
+            <motion.div
               className={`text-[12px] 2xl:text-[14px] 3xl:text-[18px]   ${showFormBox ? "text-white " : "text-[#212121]"} mb-[20px] max-w-[650px]`}
               variants={textAnimation}
               transition={{ delay: 0.6 }}
             >
               {(description && renderHtml(description)) || ""}
-            </motion.p>
+            </motion.div>
           </motion.div>
 
           {/* formBox */}
--- src/components/features/about/ContactusSection.jsx
+++ src/components/features/about/ContactusSection.jsx
@@ -9,7 +9,7 @@ export default function ContactusSection({
     return (
         <section className="w-full relative py-[45px]">
             <div className="container">
-                <div class="flex flex-wrap relative w-full h-full bg-gradient-to-r from-[rgba(230,241,255,1)] to-[rgba(230,241,255,0.63)]
+                <div className="flex flex-wrap relative w-full h-full bg-gradient-to-r from-[rgba(230,241,255,1)] to-[rgba(230,241,255,0.63)]
                  p-[15px_20px] xl:p-[20px_25px] 2xl:p-[25px_30px] 3xl:p-[30px_40px] rounded-[3px] min-h-[220px] xl:min-h-[275px] 2xl:min-h-[320px] 3xl:min-h-[410px]">
                     <div className=" w-full sm:w-1/2 flex items-center">
                         <div className="w-auto max-w-[620px]">
@@ -19,7 +19,7 @@ export default function ContactusSection({
                             <Heading size="heading1" as="div" className="leading-none">
                                 {about_cms?.section9_title}
                             </Heading>
-                            <p>{parse(about_cms?.section9_description)}</p>
+                            {parse(about_cms?.section9_description)}
                             <Link href="/contact" className="bg-[#671448] btn-base1 hover min-w-[110px] 2xl:min-w-[130px] 3xl:min-[165px]" aria-label="apply_btn">CONTACT US</Link>
                         </div>
                     </div>
