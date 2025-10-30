d89513e43e64cc366aff69000101d4ecbd61a57f
afsalmern <afsal@intersmart.in>
2025-10-30 13:56:36 +0530

    test

--- src/components/layout/Header/HeaderClient.jsx
+++ src/components/layout/Header/HeaderClient.jsx
@@ -228,7 +228,6 @@ export default function HeaderClient({ site_settings, social_links, services, re
                               ))}
                             </ul>  */}
 
-                            {/* logo section */}
                             <Link
                               href="#"
                               className="flex items-center justify-center w-full h-full max-w-[200px] 3xl:max-w-[250px] mt-[25px]"
