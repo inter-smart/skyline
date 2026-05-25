066fd13ab6735d0b199c35cd567f44fdec026de4
afsalmern <afsal@intersmart.in>
2026-05-25 11:05:15 +0530

    un commented whatsapp

--- src/components/common/WidgetSection.jsx
+++ src/components/common/WidgetSection.jsx
@@ -59,7 +59,7 @@ export default function WidgetSection({ site_settings }) {
 
           <li className="mb-[6px]">
             <div
-              // href={`http://wa.me/${site_settings?.whatsapp_number}`}
+              href={`http://wa.me/${site_settings?.whatsapp_number}`}
               className="w-[30px] 2xl:w-[35px] 3xl:w-[45px] h-[30px] 2xl:h-[35px] 3xl:h-[45px] rounded-[2px] flex items-center justify-center bg-[#0BC349] group"
               aria-label="social_icons"
               target="_blank"
